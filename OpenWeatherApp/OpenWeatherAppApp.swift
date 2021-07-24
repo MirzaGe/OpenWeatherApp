7ab2fadc0bbc63d2484d1238369d0290443527a1
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-25 04:23:43 +0500

    Initial Commit

---
+++ OpenWeatherApp/OpenWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppApp.swift
+//  OpenWeatherApp
+//
+//  Created by sherry on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
