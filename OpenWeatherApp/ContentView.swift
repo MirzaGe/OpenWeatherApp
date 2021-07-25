a4c1e1de9d89448e0ad7f09f1736e991c3f06703
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-25 06:09:38 +0500

    no errors

--- OpenWeatherApp/ContentView.swift
+++ OpenWeatherApp/ContentView.swift
@@ -16,13 +16,32 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            
+            TextField("Enter city name", text: self.$weatherVM.cityName, onCommit:  {
+                self.weatherVM.search()
+            }).font(.custom("Arial", size: 50))
+                .padding()
+                .fixedSize()
+            
+            Text(self.weatherVM.temperature)
+            .font(.custom("Arial", size: 100))
+                           
+                           .foregroundColor(Color.white)
+                           .offset(y: 100)
+                           .padding()
+            
+            
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(Color.green)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
